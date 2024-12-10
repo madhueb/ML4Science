e852de352b07a2c8aae92cb8dc9a19dfcb0f4f08
jrenard <julia.renard@epfl.ch>
2024-12-10 17:08:14 +0100

    graph for latent similarity

--- src/MIL/DeepGraphConv.py
+++ src/MIL/DeepGraphConv.py
@@ -13,19 +13,23 @@ from torch_geometric.nn import GCNConv, GraphConv, GatedGraphConv, GATConv, SGCo
 from torch_geometric.nn import GraphConv, TopKPooling, SAGPooling
 from torch_geometric.nn import global_mean_pool as gavgp, global_max_pool as gmp, global_add_pool as gap
 from torch_geometric.transforms.normalize_features import NormalizeFeatures
-from MIL_layers import Att_Net_Gated_Dual
+from src.MIL.ABMIL import Att_Net_Gated_Dual
+from src.MIL.WSI_Construction_Graph import Hnsw
+from itertools import chain
+from src.utils import *
 #from .patchgcn_utils.model_utils import *
 
 
 
 class DeepGraphConv_Surv(torch.nn.Module):
     def __init__(self, edge_agg='latent', resample=0, ndim=1024, hidden_dim=256, 
-        linear_dim=256, pool=False, use_edges=False, dropout=0.25, n_classes=4):
+        linear_dim=256, pool=False, use_edges=False, dropout=0.25, n_classes=4, radius=8):
         super(DeepGraphConv_Surv, self).__init__()
         self.use_edges = use_edges
         self.resample = resample
         self.edge_agg = edge_agg
         self.pool = pool
+        self.radius=radius
         if self.resample > 0:
             self.fc = nn.Sequential(*[nn.Dropout(self.resample)])
 
@@ -50,9 +54,9 @@ class DeepGraphConv_Surv(torch.nn.Module):
         self.path_rho = self.path_rho.to(device)
         self.classifier = self.classifier.to(device)
 
-    def forward(self, feat, edge_index, edge_latent):
-
-        x = feat
+    def forward(self, feat, edge_latent, edge_index=None):
+        
+        x = feat.squeeze(0)
         if self.edge_agg == 'spatial':
             edge_index = edge_index
         elif self.edge_agg == 'latent':
@@ -86,15 +90,29 @@ class DeepGraphConv_Surv(torch.nn.Module):
         
         logits  = self.classifier(h).unsqueeze(0) # logits needs to be a [1 x 4] vector 
         Y_hat = torch.topk(logits, 1, dim = 1)[1]
-        Y_prob = F.softmax(logits, dim=1)
+        #Y_prob = F.softmax(logits, dim=1)
+        Y_prob = F.sigmoid(logits)
         
         return logits, Y_prob, Y_hat
 
-    def calculate_objective(self, X, Y, edge_index=None, edge_latent=None):
+    def compute_latent_similarity(self, X): 
+        model = Hnsw(space='l2')  # Initialize Hnsw with the 'l2' distance metric.
+        features = np.array(X.squeeze(0))
+        num_patches = features.shape[0]
+        
+        model.fit(features)  # Fit the model using embeddings.
+        a = np.repeat(range(num_patches), self.radius-1)  # Source nodes for the edges.
+        b = np.fromiter(chain(*[model.query(features[v_idx-1], topn=self.radius)[1:] for v_idx in range(num_patches)]), dtype=int)
+        edge_latent = torch.Tensor(np.stack([a, b])).type(torch.LongTensor).to_sparse() # Construct edge tensor.
+        print(f'shape edge_latent : {type(edge_latent)}')
+        return edge_latent
+
+    def calculate_objective(self, X, Y):
         Y = Y.float()
-        logits, Y_prob, _ = self.forward(X, edge_index, edge_latent)
+        edge_latent = self.compute_latent_similarity(X)
+        logits, Y_prob, _ = self.forward(X, edge_latent)
         Y_prob = torch.clamp(Y_prob, min=1e-5, max=1. - 1e-5)
-        loss = neg_log_bernouilli(Y, Y_prob[0,1])
+        loss = neg_log_bernouilli(Y, Y_prob)
         
         #we don't care about the second argument returned
         return loss, Y_prob
@@ -103,3 +121,4 @@ class DeepGraphConv_Surv(torch.nn.Module):
         _, _, Y_hat = self.forward(X, edge_index, edge_latent)
         error = torch.mean((Y_hat != labels).float())
         return error, Y_hat
+        
